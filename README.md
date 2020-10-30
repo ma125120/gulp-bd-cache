2060b679250eb3ac47a3f34c74317c9b320c53e1
mapengda <pengda.ma@weimob.com>
2020-10-30 10:24:55 +0800

    fix: 添加缓存清除

--- README.md
+++ README.md
@@ -9,7 +9,29 @@ npm i gulp-bd-cache -D
 ## 使用
 
 ```
+/**
+  * 对目标目录作缓存，存在更改时才编译
+  * @param {boolean} open 是否开启缓存，默认true，false的时候只记录时间，不操作文件
+  * @param {number} delayWrite 等待多少ms，再写入到硬盘
+  */
 const cache = require('gulp-bd-cache');
 
 gulp.src(`app.js`).pipe(cache())
 ```
+
+## 清除缓存
+
+```
+const cache = require('gulp-bd-cache');
+
+cache.clear()
+```
+
+## Q&A
+1. cache方法的第一个参数怎么使用?
+
+答：如果需要缓存，应 **始终** 使用cache函数，如果某一次不想使用缓存，只需要将第一个参数设置为 false 即可，下次编译设置为 true 时，仍可使用缓存。
+
+2. clear方法怎么使用
+
+答：如果手动清除过目标文件夹，就需要使用 clear() 方法，否则仍会延用缓存，导致生成的文件只有变更过的文件
--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-
-const through = require('through2')
-const fs = require('fs')
-const path = require('path')
-const cacheJson = require('./cache.json')
+const through = require("through2");
+const fs = require("fs");
+const path = require("path");
+let cacheJson = {};
+try {
+  cacheJson = require("./cache.json");
+} catch (err) {}
 
 let tid;
 
- /**
-  * 对目标目录作缓存，存在更改时才编译
-  * @param {number} delayWrite 等待多少ms，再写入到硬盘
-  */
-const cacheMethod = (delayWrite = 500) => {
+/**
+ * 对目标目录作缓存，存在更改时才编译
+ * @param {boolean} open 是否开启缓存，默认true，false的时候只记录时间，不操作文件
+ * @param {number} delayWrite 等待多少ms，再写入到硬盘
+ */
+const cacheMethod = (open = true, delayWrite = 500) => {
   return through.obj((file, enc, cb) => {
-    const { path: _path, relative, } = file
-    const { mtimeMs, } = fs.statSync(_path)
-    if (cacheJson[relative]) {
+    const { path: _path, relative } = file;
+    const { mtimeMs } = fs.statSync(_path);
+    if (open && cacheJson[relative]) {
       // 文件存在更改
       if (mtimeMs > cacheJson[relative]) {
-        writeJson(relative, mtimeMs, delayWrite)
-        cb(null, file)
+        writeJson(relative, mtimeMs, delayWrite);
+        cb(null, file);
       } else {
         // 文件未更改
-        cb(null, null)
+        cb(null, null);
       }
     } else {
       // 第一次编译
-      writeJson(relative, mtimeMs)
-      cb(null, file)
+      writeJson(relative, mtimeMs);
+      cb(null, file);
     }
   });
-}
+};
 
 function writeJson(relative, mtimeMs, delayWrite) {
-  cacheJson[relative] = mtimeMs
-  clearTimeout(tid)
+  cacheJson[relative] = mtimeMs;
+  clearTimeout(tid);
   tid = setTimeout(() => {
-    fs.createWriteStream(path.resolve(__dirname, `./cache.json`)).write(JSON.stringify(cacheJson || {}))
-  }, delayWrite)
+    fs.createWriteStream(path.resolve(__dirname, `./cache.json`)).write(
+      JSON.stringify(cacheJson || {})
+    );
+  }, delayWrite);
 }
 
-module.exports = cacheMethod
+/** 清除缓存，将json文件写入空对象 */
+cacheMethod.clear = () => {
+  fs.createWriteStream(path.resolve(__dirname, `./cache.json`)).write(
+    JSON.stringify({})
+  );
+};
+
+module.exports = cacheMethod;
